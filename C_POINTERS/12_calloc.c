fb763bfff7c659bddfd249ddee2420ee983df3a3
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-04-22 18:07:08 +0530

    Added program for dynamic memory allocation using calloc

---
+++ C_POINTERS/12_calloc.c
@@ -0,0 +1,41 @@
+// Dynamic Memory Allocation using calloc
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    //allocate memory dynamically
+    p = (int *)calloc(n, sizeof(int));
+
+    if(p == NULL)
+    {
+       printf("Memory allocation failed.\n");
+       return 1;
+    }
+
+    printf("Enter array elements: ");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", (p + i));
+    }
+  
+    printf("Array elements: ");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", *(p + i));
+    }
+
+    free(p);   // release memory
+
+    return 0;
+}
+
+
+
