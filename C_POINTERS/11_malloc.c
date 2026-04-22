2ea2a8a5926ff163a10b3af4c41c899e124ac860
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-04-22 18:00:47 +0530

    Added program for dynamic memory allocation using malloc

---
+++ C_POINTERS/11_malloc.c
@@ -0,0 +1,38 @@
+// Dynamic Memory Allocation using malloc
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    //allocate memory dynamically
+    p = (int *)malloc(n * sizeof(int));
+
+    if(p == NULL)
+    {
+       printf("Memory allocation failed.\n");
+       return 1;
+    }
+
+    printf("Enter array elemnets: ");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", (p + i));
+    }
+  
+    printf("Array elements: ");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", *(p + i));
+    }
+
+    free(p);   // release memory
+
+    return 0;
+}
