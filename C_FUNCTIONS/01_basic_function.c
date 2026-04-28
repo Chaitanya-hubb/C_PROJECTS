1cc7eeee902c2147f2602eb91a7e6db089b16595
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-04-28 11:15:25 +0530

    Added basic function program in C

---
+++ C_FUNCTIONS/01_basic_function.c
@@ -0,0 +1,17 @@
+// Program to demonstrate basic function in C
+
+#include <stdio.h>
+
+// Function definition 
+void display(void)
+{
+    printf("Hello, Functions in C\n");
+}
+
+int main()
+{
+    // Function call
+    display();
+
+    return 0;
+}
