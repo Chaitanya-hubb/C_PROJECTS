dd5cff140c7cb8afdab27dc03f7760d2b4295e59
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-05-02 17:53:11 +0530

    Added basic macro program

---
+++ C_MACROS/01_basic_macro.c
@@ -0,0 +1,13 @@
+// Program to demonstrate basic macro in C
+
+#include <stdio.h>
+
+/* Macro definition */
+#define PI 3.14
+
+int main()
+{
+    printf("Value of PI = %.2f\n", PI); // 3.14
+
+    return 0;
+}
