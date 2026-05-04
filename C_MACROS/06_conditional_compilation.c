0475d4eef4e5df32808ec4e299dd7b303ad4d543
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-05-04 18:47:05 +0530

    Added conditional compilation program using #ifdef and #ifndef

---
+++ C_MACROS/06_conditional_compilation.c
@@ -0,0 +1,19 @@
+// Program to demonstrate conditional compilation using #ifdef and #ifndef
+
+#include <stdio.h>
+
+#define DEBUG
+
+int main() 
+{
+   #ifdef DEBUG
+     printf("Debug mode is ON\n");
+
+   #endif
+
+   #ifndef DEBUG
+     printf("Debuf mode is OFF\n");
+   #endif
+
+   return 0;
+}
