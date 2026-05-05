bafe858148e0e5ac13208d2a044c77767dab22dd
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-05-05 19:18:49 +0530

    Added program to swap two numbers using macro

---
+++ C_MACROS/07_swap.c
@@ -0,0 +1,21 @@
+// Program to swap two numbers using macro
+
+#include <stdio.h>
+
+#define SWAP(a, b) {int temp = a; a = b; b = temp;}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    SWAP(x, y);
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
