1afb42556483f18ede79dab2ad426dde5a459224
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-05-28 12:22:37 +0530

    Added program to count number of set bits using bitwise operations

---
+++ C_BIT_MANIPULATION/06_count_set_bits.c
@@ -0,0 +1,25 @@
+// C program to count set bits
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int num;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%u", &num);
+
+    while(num > 0)
+    {
+       if(num & 1)
+       {
+           count++;
+       }
+       num = num >> 1;
+    }
+
+    printf("Number of set bits: %d\n", count);
+
+    return 0;
+}
